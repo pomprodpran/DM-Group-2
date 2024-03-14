5ece23d2213ef311db7815ef3f69a85c7fde382c
prodpran omarak <pomprodpran@hotmail.com>
2024-03-14 00:28:57 +0000

    Automation for initial load

--- main/Transformation.R
+++ main/Transformation.R
@@ -1,23 +1,60 @@
-#library(RSQLite)
+library(RSQLite)
+library(readr)
+library(lubridate)
 
-# Rscript R/transformation.R
-# load the data
-# print("loading")
-# data_files <- list.files("data_uploads", pattern = "MOCK_DATA_PRODUCTS", file.names = TRUE)
-# data_to_write <- data.frame()
-# 
-# # for each csv
-# for(file in data_files) {
-#   print(paste("Reading file:",file))
-#   this_file_rows <- read.csv(file, stingsAsFactors = FALSE)
-#   data_to_write <- rbind(data_to_write, this_file_rows)
-# }
-# print("writing them to database")
-# 
-# connection <- RSQLITE::dbConnect(RSQLite::SQLite(), "database/product.db")
-# RSQLITE::dbWriteTable(connection,"products", data_to_write,overwrite = TRUE)
-# RSQLITE::dbDisconnect(connection)
-# print("Done!")
 
-print("Transformation file")
+# Incremental Load
+print("Loading CSV file")
 
+# Get only NEW file that has been loaded into the folder (and run historical back 2 days)
+# File format for automation: <table name>_YYYY-MM-DDTHHMMSS.csv
+current_date <- Sys.Date()
+all_files <- list.files("./data_upload", full.names = FALSE, pattern = "_")
+for (variable in all_files) {
+  file_name <- unlist(strsplit(gsub(".csv","",variable), "_")) # split file name using _ separator
+  table_name <- file_name[1]
+  date_time_parts <- unlist(strsplit(file_name[2], "T"))  # Splitting file name using 'T' separator
+  date_str <- date_time_parts[1]  # Date string
+  time_str <- date_time_parts[2]  # Time string
+  date_value <- lubridate::ymd(date_str) # Parsing date strings into datetime objects using lubridate
+  
+  if (date_value>= current_date-1 && date_value<= current_date ) {
+    print(paste("Reading file:",variable))
+    this_filepath <- paste0("./data_upload/",variable)
+    this_file_contents <- readr::read_csv(this_filepath)
+    # Add Validation !!
+    print(paste("Writing table to database:", table_name))
+    
+    my_db <- RSQLite::dbConnect(RSQLite::SQLite(),"./database/ecommerse.db")
+    
+    # Get the primary key column names from the database
+    query <- paste("SELECT name FROM pragma_table_info(", "'", table_name, "'", ") WHERE pk = 1;")
+    primary_key_columns <- dbGetQuery(my_db, "SELECT name FROM pragma_table_info('customers') where pk =1;")
+    
+    # Validation and Writing on each row to DB
+    for (i in 1:nrow(this_file_contents)) {
+      row <- this_file_contents[i, ]
+      
+      # Extract primary key values from the row
+      primary_key_values <- paste(names(row)[names(row) %in% primary_key_columns], row[names(row) %in% primary_key_columns], sep = "=", collapse = " AND ")
+      
+      # Find if the primary key exists
+      query <- paste("SELECT * FROM", table_name, paste("WHERE", primary_key_values))
+      existing_row <- dbGetQuery(my_db, query)
+      
+      if (nrow(existing_row) == 0) {
+        # Row is unique, append to the table
+        print(paste("Append:",primary_key_values))
+        dbWriteTable(my_db, table_name, row, append = TRUE)
+      } else {
+        # Row already exists, update the existing row
+        print(paste("Update:",primary_key_values))
+        update_query <- paste("UPDATE", table_name, paste("SET", paste(names(row), "=", paste0("'", row, "'"), collapse = ", "), "WHERE", primary_key_values))
+        dbExecute(my_db, update_query)
+      }
+    }
+  }
+}
+print("Done!")
+
+RSQLite::dbDisconnect(my_db)
